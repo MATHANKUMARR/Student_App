b4fe1ec60545613128e81e2524a2e64f164026ae
Mathankumar R <rmathankumarcpm@gmail.com>
2022-10-17 19:52:03 +0530

    initial commit

---
+++ src/main/java/com/example/demo/Model/Student.java
@@ -0,0 +1,67 @@
+package com.example.demo.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name="students")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	private String address;
+	private String email;
+
+	public Student(String name, String address, String email) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.email = email;
+	}
+	
+	public Student() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
